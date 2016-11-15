0881fed6f36caa76efc5d0d36019303eea7844c9
PythonicNerd <neilaylor@hotmail.com>
2016-11-14 20:53:04 -0600

    Made NarutoList better

--- changeImg.js
+++ changeImg.js
@@ -2,8 +2,7 @@ var images = document.getElementsByTagName("img");
 
 var imgLen = images.length;
 
-var narutoList = ["https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQf10GL2pqEhnpUZci2auGNvu6KKzB6oioToCNaG0BLiOd7P2d94A","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcStJisKBRMCVYL5m3ZVXjfRUD6Mkh98MOThPTbKIeuv76bhIVw2","https://s-media-cache-ak0.pinimg.com/originals/2f/f5/32/2ff53206b1ec6cbbdae926b457367ef3.jpg","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTpRSwXPvLoBI50muETDEFPO32obKollu2F9KnFDAC-pB7EhfUT","http://images.techtimes.com/data/images/full/265071/a-younger-naruto-png.png?w=760","http://i.onionstatic.com/avclub/5511/64/16x9/960.jpg"]
-
+var narutoList = ["https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQf10GL2pqEhnpUZci2auGNvu6KKzB6oioToCNaG0BLiOd7P2d94A","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcStJisKBRMCVYL5m3ZVXjfRUD6Mkh98MOThPTbKIeuv76bhIVw2","https://s-media-cache-ak0.pinimg.com/originals/2f/f5/32/2ff53206b1ec6cbbdae926b457367ef3.jpg","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTpRSwXPvLoBI50muETDEFPO32obKollu2F9KnFDAC-pB7EhfUT"]
 
 for(var i = 0; i <= imgLen; i++){
   var foreverTrue = true;
